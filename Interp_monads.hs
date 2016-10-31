7d1a98bd578f68760d1970122151562306a0abf6
dizhong <dizhong16@gmail.com>
2016-10-31 12:23:05 -0400

    a

--- Interp_monads.hs
+++ Interp_monads.hs
@@ -1,27 +1,28 @@
 module Interp_Monads where
 -- Monads
 
-data Term  = Num Int
+data Term  = Num  Int
            | Bool Bool
-           | Var String
-           | Add Term Term
-           | Sub Term Term
-           | Gt  Term Term
-           | If  Term Term Term
-           | Lam String Term
-           | App Term Term
-           | Put Int
+           | Var  String
+           | Add  Term Term
+           | Sub  Term Term
+           | Gt   Term Term
+           | If   Term Term Term
+           | Lam  String Term
+           | App  Term Term
+--         | Put
            | Get
+
      deriving (Show, Eq)
 
 data Value  = I { nOf :: Int }
             | B { bOf :: Bool }
-            | F { fOf :: Value -> Value }
+            | F { fOf :: Value -> State Value Value }
 
 
 type Env = [(String, Value)]
 
-lookupEnv                      :: String -> Env  -> Value
+lookupEnv                      :: String -> Env -> Value
 lookupEnv s []                 = error ("Value not found")
 lookupEnv s ((s', v):rest_env) = if s == s' then v else lookupEnv s rest_env
 
@@ -33,8 +34,8 @@ extendEnv s v env = (s, v):env
 type State s a = s -> (a, s)
 
 -- Get
-read :: State s s
-read = \s -> (s, s)
+my_read :: State s s
+my_read = \s -> (s, s)
 
 -- Put
 write   :: s -> State s ()
@@ -57,19 +58,22 @@ interp_m (Bool b) _        = ret (B b)
 interp_m (Var v) env       = ret (lookupEnv v env)
 interp_m (If e0 e1 e2) env = my_bind (interp_m e0 env)
                                      (\v0 -> interp_m (if bOf v0 then e1 else e2) env)
-interp_m (Add e1 e2) env   = (my_bind (interp_m e1 env)
-                                      (\v0 -> (my_bind (interp_m e2 env)
-                                                       (\v1 -> (ret (I (nOf v0 + nOf v1)))))))
-interp_m (Sub e1 e2) env   = (my_bind (interp_m e1 env)
-                                      (\v0 -> (my_bind (interp_m e2 env)
-                                                       (\v1 -> (ret (I (nOf v0 - nOf v1)))))))
-interp_m (Gt e1 e2) env    = (my_bind (interp_m e1 env)
-                                      (\v0 -> (my_bind (interp_m e2 env)
-                                                       (\v1 -> (ret (B (nOf v0 > nOf v1)))))))
---terp_m (Lam v e0) env    = (\a -> (my_bind (interp_m e0 (extendEnv v a env))
---                                   (\v0 -> v0)
--- interp_m (App e0 e1) env   = my_bind (interp_m e0 env)
---                                      (\v0 -> fOf v0 (interp_m e1 env))
+interp_m (Add e1 e2) env   = my_bind (interp_m e1 env)
+                                     (\v0 -> (my_bind (interp_m e2 env)
+                                                      (\v1 -> (ret (I (nOf v0 + nOf v1))))))
+interp_m (Sub e1 e2) env   = my_bind (interp_m e1 env)
+                                     (\v0 -> (my_bind (interp_m e2 env)
+                                                      (\v1 -> (ret (I (nOf v0 - nOf v1))))))
+interp_m (Gt e1 e2) env    = my_bind (interp_m e1 env)
+                                     (\v0 -> (my_bind (interp_m e2 env)
+                                                      (\v1 -> (ret (B (nOf v0 > nOf v1))))))
+interp_m (Lam v e0) env    = (ret (F (\a -> (my_bind (interp_m e0 (extendEnv v a env))
+                                                     (\v0 -> ret v0)))))
+interp_m (App e0 e1) env   = my_bind (interp_m e0 env)
+                                     (\v0 -> my_bind (interp_m e1 env)
+                                                     (\v1 -> ret (fOf v0 v1)))
+--interp_m (Get) env         = my_read
+--interp_m (Put i) env       = write i
 
 
 -- interp_m (Add e1 e2) env   = I (nOf (interp_m e1 env) + nOf (interp_m e2 env))
